58f124a17325f346349d8c64cc71ca2fc817b487
beefybroccoli <tedkimemail@yahoo.com>
2021-09-02 02:15:37 -0700

    no comment

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -9,7 +9,7 @@ const my_username = "beefybroccoli";
 const api_data = axios
   .get(API_URL + my_username)
   .then((response) => {
-    console.log(response.data);
+    // console.log(response.data);
     return response.data;
   })
   .catch((error) => {
@@ -29,10 +29,20 @@ const api_data = axios
     and append the returned markup to the DOM as a child of .cards
 */
 
-if (api_data) {
-  const my_card = Create_User_Card(api_data);
-  const div_cards = document.querySelector("cards");
-  div_cards.appendChild(my_card);
+if (api_data !== null) {
+  // console.log("api_data = ", api_data);
+  api_data.then((data) => {
+    //console log data
+    console.log("data = ", data);
+
+    //create a user card
+    const my_card = Create_User_Card(data);
+    console.log(`line 40 my_card = `, my_card);
+
+    //add to DOM tree
+    const div_cards = document.querySelector(".cards");
+    div_cards.appendChild(my_card);
+  });
 }
 
 /*
@@ -68,7 +78,7 @@ const followersArray = [];
     </div>
 */
 
-const Create_User_Card = ({
+function Create_User_Card({
   avatar_url,
   login,
   html_url,
@@ -76,14 +86,16 @@ const Create_User_Card = ({
   location,
   followers,
   following,
-}) => {
+}) {
+  //--------------------------------------------------------------------------
   //Step 1 - create tags
+  //--------------------------------------------------------------------------
   // <div class="card">
   const div_card = create_tag({ type: "div", classArray: ["card"] });
   // <img src={image url of user} />
   const tag_img_ = create_tag({ type: "img", src: avatar_url });
   // <div class="card-info">
-  const div_cardInfo = create_tag({ type: "div", class: ["card-info"] });
+  const div_cardInfo = create_tag({ type: "div", classArray: ["card-info"] });
   // <h3 class="name">{users name}</h3>
   const tag_h3 = create_tag({
     type: "h3",
@@ -122,7 +134,9 @@ const Create_User_Card = ({
   // <p>Bio: {users bio}</p>
   const tag_p_bio = create_tag({ type: "p", textContent: "Bio: " + bio });
 
+  //--------------------------------------------------------------------------
   //Step 2 - add childs to parent
+  //--------------------------------------------------------------------------
   /*
     <p>Profile:
       <a href={address to users github page}>{address to users github page}</a>
@@ -163,9 +177,13 @@ const Create_User_Card = ({
     div_card.appendChild(child);
   });
 
+  console.log(`line 176, div_card = `, div_card);
+
+  //--------------------------------------------------------------------------
   //Step 3 - return tag
+  //--------------------------------------------------------------------------
   return div_card;
-};
+}
 
 function create_tag({ type, textContent, classArray, src, href }) {
   const temp_tag = document.createElement(type);
