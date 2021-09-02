797dbca97a1eb0fd4af2d5cfe71b744837a2451f
beefybroccoli <tedkimemail@yahoo.com>
2021-09-02 01:45:51 -0700

    no comment

---
+++ .history/component/factory_20210902010549.js
@@ -0,0 +1,6 @@
+export function create_tag({ type, textContent, classArray,  }) {
+  const temp_tag = document.createElement(type);
+  temp_tag.textContent = textContent;
+  classArray && temp_tag.classList.add(...classArray);
+  return temp_tag;
+}
--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,5 +1,4 @@
 import axios from "axios";
-
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
@@ -7,10 +6,11 @@ import axios from "axios";
 */
 const API_URL = "https://api.github.com/users/";
 const my_username = "beefybroccoli";
-axios
+const api_data = axios
   .get(API_URL + my_username)
   .then((response) => {
     console.log(response.data);
+    return response.data;
   })
   .catch((error) => {
     console.log("error: ", error);
@@ -28,6 +28,12 @@ axios
   STEP 4: Pass the data received from Github into your function,
     and append the returned markup to the DOM as a child of .cards
 */
+
+if (api_data) {
+  const my_card = Create_User_Card(api_data);
+  const div_cards = document.querySelector("cards");
+  div_cards.appendChild(my_card);
+}
 
 /*
   STEP 5: Now that you have your own card getting added to the DOM, either
@@ -50,7 +56,7 @@ const followersArray = [];
       <img src={image url of user} />
       <div class="card-info">
         <h3 class="name">{users name}</h3>
-        <p class="username">{users user name}</p>
+        <p class="username">{users name}</p>
         <p>Location: {users location}</p>
         <p>Profile:
           <a href={address to users github page}>{address to users github page}</a>
@@ -62,6 +68,113 @@ const followersArray = [];
     </div>
 */
 
+const Create_User_Card = ({
+  avatar_url,
+  login,
+  html_url,
+  bio,
+  location,
+  followers,
+  following,
+}) => {
+  //Step 1 - create tags
+  // <div class="card">
+  const div_card = create_tag({ type: "div", classArray: ["card"] });
+  // <img src={image url of user} />
+  const tag_img_ = create_tag({ type: "img", src: avatar_url });
+  // <div class="card-info">
+  const div_cardInfo = create_tag({ type: "div", class: ["card-info"] });
+  // <h3 class="name">{users name}</h3>
+  const tag_h3 = create_tag({
+    type: "h3",
+    classArray: ["name"],
+    textContent: login,
+  });
+  // <p class="username">{users name}</p>
+  const tag_p_userName = create_tag({
+    type: "p",
+    classArray: ["username"],
+    textContent: login,
+  });
+  // <p>Location: {users location}</p>
+  const tag_p_location = create_tag({
+    type: "p",
+    textContent: `Location: ${location}`,
+  });
+  // <p>Profile: </p>
+  const tag_p_profile = create_tag({ type: "p", textContent: "Profile:" });
+  // <a href={address to users github page}>{address to users github page}</a>
+  const tag_anchor = create_tag({
+    type: "a",
+    href: html_url,
+    textContent: html_url,
+  });
+  // <p>Followers: {users followers count}</p>
+  const tag_p_followers = create_tag({
+    type: "p",
+    textContent: "Followers: " + followers,
+  });
+  // <p>Following: {users following count}</p>
+  const tag_p_follwing = create_tag({
+    type: "p",
+    textContent: "Following: " + following,
+  });
+  // <p>Bio: {users bio}</p>
+  const tag_p_bio = create_tag({ type: "p", textContent: "Bio: " + bio });
+
+  //Step 2 - add childs to parent
+  /*
+    <p>Profile:
+      <a href={address to users github page}>{address to users github page}</a>
+    </p>
+  */
+  tag_p_profile.appendChild(tag_anchor);
+
+  /*
+    <div class="card-info">
+      <h3 class="name">{users name}</h3>
+      <p class="username">{users name}</p>
+      <p>Location: {users location}</p>
+      <p>Profile: <a></a> </p>
+      <p>Followers: {users followers count}</p>
+      <p>Following: {users following count}</p>
+      <p>Bio: {users bio}</p>
+    </div>
+  */
+  [
+    tag_h3,
+    tag_p_userName,
+    tag_p_location,
+    tag_p_profile,
+    tag_p_followers,
+    tag_p_follwing,
+    tag_p_bio,
+  ].forEach((child) => {
+    div_cardInfo.appendChild(child);
+  });
+
+  /*
+    <div class="card">
+      <img src={image url of user} />
+      <div class="card-info"> ... </div>
+    </div>
+  */
+  [tag_img_, div_cardInfo].forEach((child) => {
+    div_card.appendChild(child);
+  });
+
+  //Step 3 - return tag
+  return div_card;
+};
+
+function create_tag({ type, textContent, classArray, src, href }) {
+  const temp_tag = document.createElement(type);
+  temp_tag.textContent = textContent;
+  classArray && temp_tag.classList.add(...classArray);
+  temp_tag.src = src;
+  temp_tag.href = href;
+  return temp_tag;
+}
 
 /*
   List of LS Instructors Github username's:
